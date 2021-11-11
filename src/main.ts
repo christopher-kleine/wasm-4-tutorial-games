4881a99de3b438371831a51ad5b9d2aa5d26cf1a
Christopher Kleine <chris@suletuxe.de>
2021-11-11 15:20:45 +0100

    Drawing the Snake

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
+import { Point, Snake } from "./snake"
 import * as w4 from "./wasm4";
+
+var snake = new Snake()
 
 export function start (): void {
 	store<u32>(w4.PALETTE, 0xfbf7f3, 0 * sizeof<u32>());
 	store<u32>(w4.PALETTE, 0xe5b083, 1 * sizeof<u32>());
 	store<u32>(w4.PALETTE, 0x426e5d, 2 * sizeof<u32>());
 	store<u32>(w4.PALETTE, 0x20283d, 3 * sizeof<u32>());
+
+	snake.body.push(new Point(2, 0))
+	snake.body.push(new Point(1, 0))
+	snake.body.push(new Point(0, 0))
+	snake.direction = new Point(1, 0)
 }
 
 export function update (): void {
+	snake.draw()
 }
--- src/snake.ts
+++ src/snake.ts
@@ -18,5 +18,13 @@ export class Snake {
 		this.body = new Array<Point>()
 		this.direction = new Point(1, 0)
 	}
+
+	public draw() : void {
+		store<u16>(w4.DRAW_COLORS, 0x0043)
+		this.body.forEach(part => w4.rect(part.X*8, part.Y*8, 8, 8))
+		
+		store<u16>(w4.DRAW_COLORS, 0x0004)
+		w4.rect(this.body[0].X*8,this.body[0].Y*8, 8, 8)
+	}
 }
 
