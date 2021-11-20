0ca82c7dfb5f9db0d2ebefa2010b0149ce95b9e5
MaxGraey <maxgraey@gmail.com>
2021-11-20 20:17:56 +0200

    refactorings

--- snake-as/src/main.ts
+++ snake-as/src/main.ts
@@ -1,70 +1,81 @@
 import { Point, Snake } from "./snake"
 import * as w4 from "./wasm4";
 
-var snake = new Snake()
-var frameCount = 0
-var prevState : u8
-var fruit = new Point(rnd(20), rnd(20))
-const fruitSprite = memory.data<u8>([ 0x00,0xa0,0x02,0x00,0x0e,0xf0,0x36,0x5c,0xd6,0x57,0xd5,0x57,0x35,0x5c,0x0f,0xf0 ]);
+const snake = new Snake()
+const fruit = new Point(rnd(), rnd())
+let frameCount = 0
+let prevState : u8
 
-function rnd(n : u16) : u16 {
-	return u16(Math.floor(Math.random()*n))
+const fruitSprite = memory.data<u8>([
+	0b0_0000000,
+	0b1_0100000,
+	0b0_0000010,
+	0b0_0000000,
+	0b0_0001110,
+	0b1_1110000,
+	0b0_0110110,
+	0b0_1011100,
+	0b1_1010110,
+	0b0_1010111,
+	0b1_1010101,
+	0b0_1010111,
+	0b0_0110101,
+	0b0_1011100,
+	0b0_0001111,
+	0b1_1110000
+]);
+
+function rnd(n : u16 = 20) : u16 {
+	return u16(Math.floor(Math.random() * n))
 }
 
-export function start (): void {
-	store<u32>(w4.PALETTE, 0xfbf7f3, 0 * sizeof<u32>());
-	store<u32>(w4.PALETTE, 0xe5b083, 1 * sizeof<u32>());
-	store<u32>(w4.PALETTE, 0x426e5d, 2 * sizeof<u32>());
-	store<u32>(w4.PALETTE, 0x20283d, 3 * sizeof<u32>());
+export function start(): void {
+	store<u32>(w4.PALETTE, 0xfbf7f3, 0 * sizeof<u32>())
+	store<u32>(w4.PALETTE, 0xe5b083, 1 * sizeof<u32>())
+	store<u32>(w4.PALETTE, 0x426e5d, 2 * sizeof<u32>())
+	store<u32>(w4.PALETTE, 0x20283d, 3 * sizeof<u32>())
 }
 
-function input() :void {
-	const gamepad = load<u8>(w4.GAMEPAD1);
+function input(): void {
+	const gamepad = load<u8>(w4.GAMEPAD1)
 	const justPressed = gamepad & (gamepad ^ prevState)
-	
-	if (justPressed & w4.BUTTON_UP) {
-		snake.up()
-	}
-	if (justPressed & w4.BUTTON_DOWN) {
-		snake.down()
-	}
-	if (justPressed & w4.BUTTON_LEFT) {
-		snake.left()
-	}
-	if (justPressed & w4.BUTTON_RIGHT) {
-		snake.right()
-	}
+
+	if (justPressed & w4.BUTTON_UP) snake.up()
+	if (justPressed & w4.BUTTON_DOWN) snake.down()
+	if (justPressed & w4.BUTTON_LEFT) snake.left()
+	if (justPressed & w4.BUTTON_RIGHT) snake.right()
 
 	prevState = gamepad
 }
 
-export function update (): void {
+export function update(): void {
 	frameCount++
 
 	input()
-	
+
 	if (frameCount % 15 == 0) {
 		snake.update()
 
 		if (snake.isDead()) {
-			snake.body = []
-
-			snake.body.push(new Point(2, 0))
-			snake.body.push(new Point(1, 0))
-			snake.body.push(new Point(0, 0))
-			snake.direction = new Point(1, 0)
+			snake.body = [
+				new Point(2, 0),
+				new Point(1, 0),
+				new Point(0, 0)
+			]
+			snake.direction.x = 1
+			snake.direction.y = 0
 		}
 
 		if (snake.body[0].equals(fruit)) {
-			let p = snake.body[snake.body.length-1]
-			snake.body.push(new Point(p.x, p.y))
-			fruit.x = rnd(20)
-			fruit.y = rnd(20)
+			let tail = snake.body[snake.body.length - 1]
+			snake.body.push(new Point(tail.x, tail.y))
+			fruit.x = rnd()
+			fruit.y = rnd()
 		}
 	}
 
 	snake.draw()
 
 	store<u16>(w4.DRAW_COLORS, 0x4320)
-	w4.blit(fruitSprite, fruit.x*8, fruit.y*8, 8, 8, w4.BLIT_2BPP)
+	w4.blit(fruitSprite, fruit.x * 8, fruit.y * 8, 8, 8, w4.BLIT_2BPP)
 }
