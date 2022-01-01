38f441508449d7a344805e0759c6c284f3ba1ce9
Jim Newsome <jim@sporksmith.net>
2022-01-01 14:10:19 -0600

    static mut instead of lazy_static

--- snake-rust/src/lib.rs
+++ snake-rust/src/lib.rs
@@ -5,19 +5,18 @@ mod palette;
 mod snake;
 mod wasm4;
 use game::Game;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
 
-lazy_static! {
-    static ref SNAKE_GAME: Mutex<Game> = Mutex::new(Game::new());
-}
+static mut SNAKE_GAME: Option<Game> = None;
 
 #[no_mangle]
 fn start() {
     palette::set_palette([0xfff6d3, 0xf9a875, 0xeb6b6f, 0x7c3f58]);
+    let snake = unsafe { &mut SNAKE_GAME };
+    *snake = Some(Game::new());
 }
 
 #[no_mangle]
 fn update() {
-    SNAKE_GAME.lock().expect("game_state").update();
+    let snake = unsafe { SNAKE_GAME.as_mut().unwrap() };
+    snake.update();
 }
