307761a6cf2f1bc747278a0fcb8c5c741ff493a1
Jim Newsome <jim@sporksmith.net>
2022-01-03 02:50:08 -0600

    don't bother with `wasm-strip`.
    `wasm-opt` already has the same effect if we add --strip-debug.
    `wasm-strip` also does it implicitly.

--- snake-rust/build.sh
+++ snake-rust/build.sh
@@ -7,5 +7,4 @@ cargo build --release
 cp target/wasm32-unknown-unknown/release/*.wasm optimized.wasm
 
 wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code -o optimized.wasm optimized.wasm
-wasm-strip optimized.wasm
-wasm-opt -Oz --strip-producers --dce --zero-filled-memory optimized.wasm -o optimized.wasm
+wasm-opt -Oz --strip-producers --strip-debug --dce --zero-filled-memory optimized.wasm -o optimized.wasm
