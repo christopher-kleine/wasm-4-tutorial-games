70e30518911ea3561ba99c5e45d1bd6545050c3a
MrRafael-dev <94740791+MrRafael-dev@users.noreply.github.com>
2022-01-08 15:34:20 -0300

    Update main.ts

--- maze-as/src/main.ts
+++ maze-as/src/main.ts
@@ -1,11 +1,11 @@
 import * as w4 from "./wasm4";
-import * as Game from "./game";
+import * as Maze from "./maze";
 
 /** Collision alls. */
-let walls: Game.Wall[];
+let walls: Maze.Wall[];
 
 /** Playable character. */
-let player: Game.Player;
+let player: Maze.Player;
 
 /**
  * @event start
@@ -14,20 +14,20 @@ export function start(): void {
   // Set walls for collision...
   walls = [
     // Boundaries
-    new Game.Wall(0,    0, 160,   8),
-    new Game.Wall(0,  152, 160,   8),
-    new Game.Wall(0,    8,   8, 148),
-    new Game.Wall(152,  8,   8, 148),
+    new Maze.Wall(0,    0, 160,   8),
+    new Maze.Wall(0,  152, 160,   8),
+    new Maze.Wall(0,    8,   8, 148),
+    new Maze.Wall(152,  8,   8, 148),
 
     // Corridors
-    new Game.Wall(26,  29,  16,  60),
-    new Game.Wall(62,  41,  72,   8),
-    new Game.Wall(90,  72,  16,  64),
-    new Game.Wall(34, 103,  40,  32)
+    new Maze.Wall(26,  29,  16,  60),
+    new Maze.Wall(62,  41,  72,   8),
+    new Maze.Wall(90,  72,  16,  64),
+    new Maze.Wall(34, 103,  40,  32)
   ];
 
   // Instance player and set collision walls for it...
-  player = new Game.Player(80, 80);
+  player = new Maze.Player(80, 80);
   player.walls = walls;
 }
 
@@ -43,7 +43,7 @@ export function update(): void {
 
     // Update/draw walls...
     for(let i = 0; i < walls.length; i += 1) {
-      let wall: Game.Wall = walls[i];
+      let wall: Maze.Wall = walls[i];
       wall.update();
       wall.draw();
     }
